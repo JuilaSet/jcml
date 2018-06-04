f92483cf7057380a95afa2f5c4db2237a99e3c5b
JuliaSet <1445469285@qq.com>
2018-06-04 20:28:09 +0800

    PDA accomplished
    实现了下推自动机

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,8 +1,23 @@
 package core;
 
+import tools.Pda;
+import tools.Pda.Action;
 
 public class Main {
 	public static void main(String[] arg){
-		new Parser("codes/test.jcml");
+	//	new Parser("codes/test.jcml");
+		Pda<Integer,Character> pda = new Pda<Integer,Character>();
+		pda.add(0, '(', Action.push, 1);
+		pda.add(0, '#', Action.accept);
+		pda.add(1, '(', Action.push, 1);
+		pda.add(1, ')', Action.pop);
+		pda.setStart(0);
+		pda.addEofPoint(0);
+		pda.move('(');
+		pda.move(')');
+		pda.move('(');
+		pda.move('(');
+		pda.eof();
+		System.out.println("CURSTAT:"+pda.getCurrentState()+":"+pda.isAccept());
 	}
 }
